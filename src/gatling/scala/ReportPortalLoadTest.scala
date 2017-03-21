5756215f2e1cf1fb6f07d6be1d91b14c29e5ee52
Vadzim Hushchanskou <Vadzim_Hushchanskou@epam.com>
2017-03-21 14:46:29 +0300

    Image generator was implemented

--- src/gatling/scala/ReportPortalLoadTest.scala
+++ src/gatling/scala/ReportPortalLoadTest.scala
@@ -1,15 +1,12 @@
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.concurrent.atomic.AtomicLong
-
-import io.gatling.core.Predef.Simulation
+import io.gatling.core.Predef._
 
 /**
   * Created by delgr on 19.03.2017.
   */
-class ReportPortalLoadTest extends Simulation{
+class ReportPortalLoadTest extends Simulation {
 
-
+  val usersFileName = "users.tsv"
+  val userFeeder = tsv(usersFileName).circular
 
 
 }
--- src/main/scala/com/epam/ta/reportportal/util/TestLogUtil.scala
+++ src/main/scala/com/epam/ta/reportportal/util/TestLogUtil.scala
@@ -4,43 +4,61 @@
 
 package com.epam.ta.reportportal.util
 
+import java.awt.geom.Ellipse2D
+import java.awt.image.BufferedImage
+import java.awt.{Color, Rectangle, Shape}
+import java.io.ByteArrayOutputStream
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.concurrent.ConcurrentHashMap
 import java.util.concurrent.atomic.AtomicLong
+import javax.imageio.ImageIO
 
+import org.apache.commons.lang3.RandomUtils._
 import org.apache.commons.lang3.{RandomStringUtils, StringUtils}
 import org.apache.commons.math3.distribution.GammaDistribution
 
-object LogEntryGenerator extends Iterator[String] {
-  val logTimeStampFormat = "HH:mm:ss.SSS"
-  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890 - _:<>&\"'=+/\\,.$\t\n"
 
+private object Random {
+  private val gammaDistribution = new GammaDistribution(2, 2)
+
+  def gamma(): Double = gammaDistribution.sample()
+}
+
+private object Counter {
   private val uniqueIdCounter: AtomicLong = new AtomicLong()
 
   private val threadIdCounter: AtomicLong = new AtomicLong()
 
   private val threads = new ConcurrentHashMap[Long, Long]()
 
-  private val formats = new ConcurrentHashMap[Long, SimpleDateFormat]()
+  def threadNumber(): Long = {
+    val threadId = Thread.currentThread().getId
+    if (threads.containsKey(threadId)) {
+      threads.get(threadId)
+    }
+    else {
+      val number = threadIdCounter.getAndIncrement()
+      threads.put(threadId, number)
+      number
+    }
+  }
 
-  private val random = new GammaDistribution(2, 2)
+  def uniqueId(): Long = uniqueIdCounter.getAndIncrement()
+}
+
+object LogEntryGenerator extends Iterator[String] {
+  val logTimeStampFormat = "HH:mm:ss.SSS"
+  val alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz 1234567890 - _:<>&\"'=+/\\,.$\t\n"
+
+  private val formats = new ConcurrentHashMap[Long, SimpleDateFormat]()
 
   override def hasNext: Boolean = true
 
   override def next(): String = {
-    val threadId = Thread.currentThread().getId
-    val threadNumber = {
-      if (threads.containsKey(threadId)) {
-        threads.get(threadId)
-      }
-      else {
-        val number = threadIdCounter.getAndIncrement()
-        threads.put(threadId, number)
-        number
-      }
-    }
+    val threadNumber = Counter.threadNumber()
 
+    val threadId = Thread.currentThread().getId
     val sdf: SimpleDateFormat = {
       if (formats.containsKey(threadId)) {
         formats.get(threadId)
@@ -53,7 +71,49 @@ object LogEntryGenerator extends Iterator[String] {
     }
 
     StringUtils.join(sdf.format(new Date()), " [Test thread (", threadNumber, ")] DEBUG TC", threadNumber,
-      "_VerifyTextLogging - ", uniqueIdCounter.getAndIncrement(), " - ",
-      RandomStringUtils.random(Math.round(random.sample() * 32).toInt, alphabet))
+      "_VerifyTextLogging - ", Counter.uniqueId(), " - ",
+      RandomStringUtils.random(Math.round(Random.gamma() * 32).toInt, alphabet))
+  }
+}
+
+object PictureEntryGenerator extends Iterator[Array[Byte]] {
+  private val backgroundColor = new Color(255,255,255);
+  val imageWidth = 1366
+  val imageHeight = 768
+  val minColor = 0
+  val maxColor = 256
+  val imageRect = new Rectangle(0,0,imageWidth, imageHeight)
+
+  override def hasNext: Boolean = true
+
+  override def next(): Array[Byte] = {
+    val numberOfFigures = Math.round(2 * Random.gamma()).toInt
+    val circles = nextInt(0, numberOfFigures)
+    val rectangles = numberOfFigures - circles
+    val image = new BufferedImage(imageWidth, imageHeight, BufferedImage.TYPE_INT_RGB)
+    val graphics = image.createGraphics()
+    graphics.setColor(backgroundColor)
+    graphics.fill(imageRect)
+
+    for (i <- 0 to numberOfFigures) {
+      var shape: Shape = null
+      if (i < rectangles) {
+        val x = nextInt(0, imageWidth)
+        val y = nextInt(0, imageHeight)
+        val width = nextInt(x, imageWidth + 1)
+        val height = nextInt(y, imageHeight + 1)
+        shape = new Rectangle(x, y, width, height)
+      } else {
+        val x = nextInt(0, imageWidth)
+        val y = nextInt(0, imageHeight)
+        val d = nextInt(1, Math.min(imageWidth - x, imageHeight - y))
+        shape = new Ellipse2D.Float(x, y, d, d)
+      }
+      graphics.setColor(new Color(nextInt(minColor, maxColor), nextInt(minColor, maxColor), nextInt(minColor, maxColor)))
+      graphics.fill(shape)
+    }
+    val baos = new ByteArrayOutputStream()
+    ImageIO.write(image, "png", baos)
+    baos.toByteArray
   }
 }
